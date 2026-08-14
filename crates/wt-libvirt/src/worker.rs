b33510d77251e4b7d85eb7571af6ab39e3e67bbd
Lucas Ávila <lucaxx@gmail.com>
2026-08-14 14:47:25 +0000

    refactor: extract libvirt network discovery

--- crates/wt-libvirt/src/worker.rs
+++ crates/wt-libvirt/src/worker.rs
@@ -2,11 +2,13 @@
 
 mod guest_agent;
 mod image;
+mod network;
 mod provider;
 mod world;
 
 use crate::{MachineConfig, LIBVIRT_URI};
 use image::{read_virtual_size as read_image_virtual_size, validate_disk_size};
+use network::{domain_ip, network_address};
 use std::fs;
 use std::io::Write;
 use std::ops::Deref;
@@ -605,41 +607,6 @@ pub(super) fn lookup_domain(provider_id: &ProviderId) -> Result<Domain, WorkerEr
     let connection = LibvirtConnection::open()?;
     Domain::lookup_by_name(&connection, provider_id.as_str())
         .map_err(|error| context("look up libvirt domain", error))
-}
-
-fn domain_ip(provider_id: &ProviderId) -> Result<Option<String>, WorkerError> {
-    let domain = lookup_domain(provider_id)?;
-    let interfaces = domain
-        .interface_addresses(virt::sys::VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE, 0)
-        .map_err(|error| context("get domain interface addresses", error))?;
-    Ok(interfaces
-        .into_iter()
-        .flat_map(|interface| interface.addrs)
-        .find_map(|address| {
-            let ip = address.addr.parse::<std::net::IpAddr>().ok()?;
-            (ip.is_ipv4() && !ip.is_loopback()).then(|| ip.to_string())
-        }))
-}
-
-fn network_address(connection: &Connect, name: &str) -> Result<String, WorkerError> {
-    let network = Network::lookup_by_name(connection, name)
-        .map_err(|error| context("look up libvirt network", error))?;
-    let xml = network
-        .get_xml_desc(0)
-        .map_err(|error| context("read libvirt network XML", error))?;
-    for quote in ['\'', '"'] {
-        let needle = format!("address={quote}");
-        for rest in xml.split(&needle).skip(1) {
-            if let Some(address) = rest.split(quote).next() {
-                if address.parse::<std::net::Ipv4Addr>().is_ok() {
-                    return Ok(address.to_owned());
-                }
-            }
-        }
-    }
-    Err(WorkerError::new(
-        "configured libvirt network has no IPv4 bridge address",
-    ))
 }
 
 fn require_file(path: &std::path::Path, label: &str) -> Result<(), WorkerError> {
