446df0fa7d5a36a054c1ff48623e949c39ba37e1
Lucas Ávila <lucaxx@gmail.com>
2026-08-27 21:01:40 +0000

    Remove shell open hints

--- crates/products/wt/client/src/shell/render.rs
+++ crates/products/wt/client/src/shell/render.rs
@@ -268,7 +268,7 @@ fn draw_worlds(frame: &mut Frame<'_>, area: Rect, model: &ShellModel, creation: 
                 (world.detail != "-").then_some(world.detail.as_str()),
                 &[],
                 index == model.active(),
-                "Enter or click to open",
+                "",
                 true,
             )
         });
